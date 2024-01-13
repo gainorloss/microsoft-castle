13b34a6e9d20f64cc0b9a47be12ae0391aa28b5a
gs <519564415@qq.com>
2024-01-13 19:01:27 +0800

    :white_check_mark:(benchmark)

---
+++ benchmark/MicrosoftCastle.Benchmark/ServiceProviderBuildBenchmark.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using MicrosoftCastle.ConsoleApp;
+using MicrosoftCastle.Samples.Common;
+
+namespace MicrosoftCastle.Benchmark
+{
+    [Config(typeof(Config)), MemoryDiagnoser]
+    public class ServiceProviderBuildBenchmark
+    {
+        internal class Config : ManualConfig
+        {
+
+        }
+        private IServiceCollection _services;
+        public ServiceProviderBuildBenchmark()
+        {
+            _services = new ServiceCollection();
+            _services.TryAddTransient<SampleService>();
+            _services.TryAddTransient<LoggingInterceptor>();//有依赖容器服务的拦截器，需要放到容器中
+        }
+
+        [Benchmark(Baseline = true)]
+        public IServiceProvider Normal() => _services.BuildServiceProvider();
+
+        [Benchmark]
+        public IServiceProvider DynamicProxy() => _services.ConfigureCastleDynamicProxy().BuildServiceProvider();
+    }
+}
---
+++ benchmark/MicrosoftCastle.Benchmark/ServiceResolveBenchmark.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using MicrosoftCastle.ConsoleApp;
+using MicrosoftCastle.Samples.Common;
+
+namespace MicrosoftCastle.Benchmark
+{
+    [Config(typeof(Config)), MemoryDiagnoser]
+    public class ServiceResolveBenchmark
+    {
+        internal class Config : ManualConfig
+        {
+
+        }
+        private IServiceProvider _normal;
+        private IServiceProvider _proxy;
+        public ServiceResolveBenchmark()
+        {
+            var services = new ServiceCollection();
+            services.TryAddTransient<SampleService>();
+            services.TryAddTransient<LoggingInterceptor>();//有依赖容器服务的拦截器，需要放到容器中
+            _normal = services.BuildServiceProvider();
+            _proxy = services.ConfigureCastleDynamicProxy().BuildServiceProvider();
+        }
+
+        [Benchmark(Baseline = true)]
+        public SampleService Normal() => _normal.GetRequiredService<SampleService>();
+
+        [Benchmark]
+        public SampleService DynamicProxy() => _proxy.GetRequiredService<SampleService>();
+    }
+}
